114b3f4cceb05d655f21b0042ce529d5fdb321b3
David Hoffman <dave.p.hoffman@gmail.com>
2016-03-08 14:57:47 -0500

    updated tests to test actual fit, and test data generation.

--- tests/test_gauss2d.py
+++ tests/test_gauss2d.py
@@ -1,4 +1,5 @@
 from nose.tools import *
+import warnings
 from skimage.external import tifffile as tif
 from peaks.gauss2d import Gauss2D
 import os
@@ -6,6 +7,9 @@ import numpy as np
 import unittest
 
 class TestGauss2D(unittest.TestCase):
+    '''
+    Testing the Gauss2D function in peaks.gauss2d
+    '''
 
     def setUp(self):
         self.x = np.arange(64)
@@ -13,39 +17,85 @@ class TestGauss2D(unittest.TestCase):
 
         self.xx, self.yy = np.meshgrid(self.x, self.y)
 
-        self.data = tif.imread(os.path.join(os.path.dirname(__file__),'..','fixtures','noisy_data.tif'))
+        self.noisy_data = tif.imread(os.path.join(os.path.dirname(__file__), '..',
+                                            'fixtures', 'noisy_data.tif'))
 
-        #read in test data, read in optimized fit params, read in optimized fit
-        #everything should be in tiff file format.
+        self.raw_data = tif.imread(os.path.join(os.path.dirname(__file__), '..',
+                                            'fixtures', 'raw_data.tif'))
+
+        # read in test data, read in optimized fit params, read in optimized
+        # fit everything should be in tiff file format.
 
     def test_data(self):
         '''
         Make sure object contains right data and that it returns a copy
         '''
 
-        myg = Gauss2D(self.data)
-        #check that data is the same
-        assert np.all(self.data == myg.data)
-        #but not the same object
-        assert_is_not(self.data, myg.data)
+        myg = Gauss2D(self.noisy_data)
+        # check that data is the same
+        assert np.all(self.noisy_data == myg.data)
+        # but not the same object
+        assert_is_not(self.noisy_data, myg.data)
 
     def test_estimate_params(self):
-        myg = Gauss2D(self.data)
-        assert np.allclose(myg.estimate_params(), np.array([  1.24887055e+00, \
-            6.42375215e+01,   3.07687388e+01, 3.54327311e+01,   1.77964115e+01,\
-            1.02789770e-02, 8.30703795e-01]))
+        '''
+        Test estimate params
+        '''
+        myg = Gauss2D(self.noisy_data)
+        assert np.allclose(myg.estimate_params(),
+                           np.array([1.24887055e+00,
+                                     6.42375215e+01,
+                                     3.07687388e+01,
+                                     3.54327311e+01,
+                                     1.77964115e+01,
+                                     1.02789770e-02,
+                                     8.30703795e-01]))
 
+    def test_fit(self):
+        '''
+        Test fit of test data
+        '''
+        myg = Gauss2D(self.noisy_data)
+        with warnings.catch_warnings():
+            # we know we'll get a warning (see test below)
+            warnings.simplefilter("ignore", UserWarning)
+            myg.optimize_params(modeltype='full')
+
+        assert np.allclose(myg.opt_params,
+                           np.array([0.99788977,
+                                     70.01566965,
+                                     25.00790577,
+                                     19.99506797,
+                                     9.99125883,
+                                     0.49377357,
+                                     1.0001033]))
+
+    def test_gen_model(self):
+        '''
+        Test model
+        '''
+        params =  np.array([1,
+                            70,
+                            25,
+                            20,
+                            10,
+                            0.5,
+                            1])
+        myg = Gauss2D.gen_model(self.noisy_data, *params)
+        assert np.allclose(self.raw_data, myg)
 
     def test_rho(self):
         '''
         Test that rho is rejected if outside acceptable range
         '''
 
-        coefs_full = [12,20,70,20,30,-1,2]
-        assert_raises(ValueError, Gauss2D.gauss2D, (self.xx, self.yy),*coefs_full)
+        coefs_full = [12, 20, 70, 20, 30, -1, 2]
+        assert_warns(UserWarning, Gauss2D.gauss2D, (self.xx, self.yy),
+                     *coefs_full)
 
-        coefs_full = [12,20,70,20,30,1,2]
-        assert_raises(ValueError, Gauss2D.gauss2D, (self.xx, self.yy),*coefs_full)
+        coefs_full = [12, 20, 70, 20, 30, 1, 2]
+        assert_warns(UserWarning, Gauss2D.gauss2D, (self.xx, self.yy),
+                     *coefs_full)
 
     def test_unequal_range(self):
         '''
@@ -57,21 +107,21 @@ class TestGauss2D(unittest.TestCase):
 
         xx, yy = np.meshgrid(x, y)
 
-        coefs_full = [12,20,70,20,30,0.5,2]
+        coefs_full = [12, 20, 70, 20, 30, 0.5, 2]
 
-        assert_raises(ValueError, Gauss2D.gauss2D,(self.xx, yy),*coefs_full)
-        assert_raises(ValueError, Gauss2D.gauss2D,(xx, self.yy),*coefs_full)
+        assert_raises(ValueError, Gauss2D.gauss2D, (self.xx, yy), *coefs_full)
+        assert_raises(ValueError, Gauss2D.gauss2D, (xx, self.yy), *coefs_full)
 
     def test_gauss2D_sym(self):
         '''
         Testing if the symmetrical case is a sub case of the full one
         '''
 
-        coefs_full = [12,20,70,20,20,0,2]
-        full_data = Gauss2D.gauss2D((self.xx, self.yy),*coefs_full)
+        coefs_full = [12, 20, 70, 20, 20, 0, 2]
+        full_data = Gauss2D.gauss2D((self.xx, self.yy), *coefs_full)
 
         coefs_sym = coefs_full[:4]+[coefs_full[-1]]
-        sym_data = Gauss2D.gauss2D_sym((self.xx, self.yy),*coefs_sym)
+        sym_data = Gauss2D.gauss2D_sym((self.xx, self.yy), *coefs_sym)
 
         assert np.all(sym_data == full_data)
 
@@ -80,11 +130,11 @@ class TestGauss2D(unittest.TestCase):
         Testing if the no rotation case is a sub case of the full one
         '''
 
-        coefs_full = [12,20,70,20,30,0,2]
-        full_data = Gauss2D.gauss2D((self.xx, self.yy),*coefs_full)
+        coefs_full = [12, 20, 70, 20, 30, 0, 2]
+        full_data = Gauss2D.gauss2D((self.xx, self.yy), *coefs_full)
 
         coefs_norot = coefs_full[:5]+[coefs_full[-1]]
-        norot_data = Gauss2D.gauss2D_norot((self.xx, self.yy),*coefs_norot)
+        norot_data = Gauss2D.gauss2D_norot((self.xx, self.yy), *coefs_norot)
 
         assert np.all(norot_data == full_data)
 
@@ -93,110 +143,17 @@ class TestGauss2D(unittest.TestCase):
         Test model classmethod in Gauss2D class
         '''
 
-        #first test acceptable values for goodness
-        funcs = (Gauss2D.gauss2D,Gauss2D.gauss2D_norot,Gauss2D.gauss2D_sym)
-        coefs = ([12,20,70,20,30,0.5,2], [12,20,70,20,30,2], [12,20,70,20,2])
+        # first test acceptable values for goodness
+        funcs = (Gauss2D.gauss2D, Gauss2D.gauss2D_norot, Gauss2D.gauss2D_sym)
+        coefs = ([12, 20, 70, 20, 30, 0.5, 2],
+                 [12, 20, 70, 20, 30, 2],
+                 [12, 20, 70, 20, 2])
 
         for func, coef in zip(funcs, coefs):
-            data = func((self.xx, self.yy),*coef)
-            model = Gauss2D.model((self.xx, self.yy),*coef)
+            data = func((self.xx, self.yy), *coef)
+            model = Gauss2D.model((self.xx, self.yy), *coef)
             assert np.all(model == data)
 
-        #now test edge cases
+        # now test edge cases
         assert_raises(ValueError, Gauss2D.model, 1)
         assert_raises(ValueError, Gauss2D.model, *np.arange(10))
-
-
-
-
-# import matplotlib.pylab as plt
-# import numpy as np
-#
-# from gauss2d import Gauss2D
-#
-# def plotdata(data, title):
-#     fig, ax = plt.subplots()
-#     ax.matshow(data)
-#     ax.contour(data, 8, colors='k')
-#     ax.set_title(title)
-#     fig.show()
-#     fig.tight_layout()
-#
-# def test_gauss2D():
-#     #generate some shitty data
-#     x = np.arange(64)
-#     y = np.arange(128)
-#
-#     xx, yy = np.meshgrid(x, y)
-#
-#     coefs = [12,20,70,10,20,0.5,2]
-#
-#     data = Gauss2D.gauss2D((xx, yy),*coefs)
-#
-#     plotdata(data,'test_gauss2D')
-#
-# def test_gauss2D_norot():
-#     #generate some shitty data
-#     x = np.arange(64)
-#     y = np.arange(128)
-#
-#     xx, yy = np.meshgrid(x, y)
-#
-#     coefs = (12,20,70,10,20,2)
-#
-#     data = Gauss2D.gauss2D_norot((xx, yy),*coefs)
-#
-#     plotdata(data,'test_gauss2D_norot')
-#
-# def test_gauss2D_norot_fit():
-#     #generate some shitty data
-#     x = np.arange(64)
-#
-#     y = np.arange(128)
-#
-#     xx, yy = np.meshgrid(x, y)
-#
-#     theta = 20*np.pi/180
-#
-#     xxr = (xx-30)*np.cos(theta)+(yy-90)*np.sin(theta)
-#     yyr = (xx-30)*np.sin(theta)+(yy-90)*np.cos(theta)
-#     data = 5+5*np.exp(-xxr**2/2/10**2-yyr**2/2/25**2)
-#
-#     noisy_data = Gauss2D(data+0.2*np.random.randn(*xx.shape))
-#
-#     guessparams = noisy_data.estimate_params()
-#
-#     noisy_data.optimize_params(guessparams)
-#
-#     noisy_data_estimate = Gauss2D.model((xx,yy),*noisy_data.get_guess_params())
-#
-#     noisy_data_fit = Gauss2D.model((xx,yy),*noisy_data.get_opt_params())
-#
-#     fig, ax = plt.subplots()
-#     ax.matshow(noisy_data._data)
-#     ax.contour(noisy_data_estimate, 8, colors='r')
-#     ax.contour(noisy_data_fit, 8, colors='k')
-#     ax.set_title('Optimization Test')
-#     fig.show()
-#
-# def test_gauss2D_sym():
-#     #generate some shitty data
-#     x = np.arange(64)
-#     y = np.arange(128)
-#
-#     xx, yy = np.meshgrid(x, y)
-#
-#     coefs = (12,20,70,10,2)
-#
-#     data = Gauss2D.gauss2D_sym((xx, yy),*coefs)
-#
-#     plotdata(data,'test_gauss2D_sym')
-#
-# if __name__ == '__main__':
-#     test_gauss2D()
-#     test_gauss2D_norot()
-#     test_gauss2D_sym()
-#     test_gauss2D_norot_fit()
-#
-#     #blocking to show results
-#     plt.show()
